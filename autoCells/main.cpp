38c346cd94262f92645949ae3d000991e24a58d5
ramenkup <sklinge@email.arizona.edu>
2015-04-05 15:46:47 -0700

    Initial Commit

---
+++ autoCells/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  autoCells
+//
+//  Created by Spencer Klinge on 4/5/15.
+//  Copyright (c) 2015 Spencer Klinge. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
